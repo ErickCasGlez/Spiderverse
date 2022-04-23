01dbb0850073c75b92e3ca8ee3347fde8320cadf
ErickCasGlez <erick.cas.glez@gmail.com>
2022-04-22 21:11:06 -0500

    Modificación
    Se cambian los datos en el archivo de prueba

--- test/spiderverse.test.js
+++ test/spiderverse.test.js
@@ -1,6 +1,15 @@
 describe("Test Suite Dummy Description", () => {
     test('Case 1 Dummy', () => {
-        const resultOfSomething = 1 + 2
-        expect(resultOfSomething).toBe(88);
+
+        //Aquí va el código que queremos ver el resultado
+        //Se busca un objeto con estos datos
+        const andrewGarfield = Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
+
+        //Validamos que el código nos arroje el resultado esperado
+        expect(andrewGarfield.name).toBe("Spiderman Sony")
+        expect(andrewGarfield.age).toBe(31)
+        expect(andrewGarfield.actor).toBe("Andrew Garfield")
+        expect(andrewGarfield.movies).toBe(31)
+        expect(andrewGarfield.studio).toBe("Sony")
     });
 })
